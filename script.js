174d366b345cfd6f1afe55faf5056b270a09e597
Céleste Robinson <celestekrobinson@gmail.com>
2017-09-22 10:53:30 -0600

    test

--- script.js
+++ script.js
@@ -48,11 +48,6 @@ function getUserInput(story) {
     injectWords(nounWords, 'noun', story);
 } 
 
-// Selects value of input fields and turns input into an array at commas
-// is used to fill in spans in stories (for loop)
-// If array has less than 4 values, select a random value from data.js arrays to fill
-//in the remaining values
-
 function injectWords(arr, wordType, story) {
     var spots = $('#' + story + ' .' + wordType);
     for (var i = 0; i < 4; i++) {
@@ -61,47 +56,47 @@ function injectWords(arr, wordType, story) {
 }
 
 function createRandWordArr() {
-var randNounArr = [];
-var randAdjArr = [];
-var randVerbArr = [];
-var randAdverbArr = [];
+var verbInput = $('#verb-input'); //grabs input from all textboxes
+var adjectiveInput = $('#adjective-input');
+var adverbInput = $('#adverb-input');
+var nounInput = $('#noun-input');
+var verbWords = verbInput.val().split(','); //breaks them into arrays on the comma
+var adjectiveWords = adjectiveInput.val().split(',');
+var adverbWords = adverbInput.val().split(',');
+var nounWords = nounInput.val().split(',');
+var randNounArr = nounWords;
+var randAdjArr = adjectiveWords;
+var randVerbArr = verbWords;
+var randAdverbArr = adverbWords;
 var story = getStory();
 //Nouns
     while(randNounArr.length < 4) {
-        if($('#noun-input').val = (' ')) {
-            for (var i = 0; i < 4; i++) {
-            randNounArr.push(randFilteredWord(nouns, randNounArr));
-            }
+        for (var i = 0; i < 4; i++) {
+        randNounArr.push(randFilteredWord(nouns, randNounArr));
         }
     }
     console.log(randNounArr);
     injectWords(randNounArr, 'noun', story)
 //Adjectives
     while(randAdjArr.length < 4) {
-        if($('#adjective-input').val = (' ')) {
-            for (var i = 0; i < 4; i++) {
-            randAdjArr.push(randFilteredWord(adjectives, randAdjArr));
-            }
+        for (var i = 0; i < 4; i++) {
+        randAdjArr.push(randFilteredWord(adjectives, randAdjArr));
         }
     }
     console.log(randAdjArr);
     injectWords(randAdjArr, 'adjective', story)
 //Verbs
     while(randVerbArr.length < 4) {
-        if($('#verb-input').val = (' ')) {
-            for (var i = 0; i < 4; i++) {
-            randVerbArr.push(randFilteredWord(verbs, randVerbArr));
-            }
+        for (var i = 0; i < 4; i++) {
+        randVerbArr.push(randFilteredWord(verbs, randVerbArr));
         }
     }
     console.log(randVerbArr);
     injectWords(randVerbArr, 'verb', story)
 //Adverbs
     while(randAdverbArr.length < 4) {
-        if($('#adverb-input').val = (' ')) {
-            for (var i = 0; i < 4; i++) {
-            randAdverbArr.push(randFilteredWord(adverbs, randAdverbArr));
-            }
+        for (var i = 0; i < 4; i++) {
+        randAdverbArr.push(randFilteredWord(adverbs, randAdverbArr));
         }
     }
     console.log(randAdverbArr);
